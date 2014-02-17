e69c2aeb19fb1ba553e62b17949e76e13bbeecaa
Maria Neise <maria.neise1@gmail.com>
2014-02-17 19:14:25 +1300

    Test for window size for valid point.

--- corner_detection_test.go
+++ corner_detection_test.go
@@ -5,6 +5,7 @@ import "path/filepath"
 import "testing"
 
 func TestLoadImage(t *testing.T) {
+
 	path := filepath.FromSlash("resources/images/5x5-clear.png")
 	i := LoadImage(path)
 	if i == nil {
@@ -22,5 +23,14 @@ func TestWindowAt(t *testing.T) {
 	if success {
 		t.Errorf("window at %v should not exist, but got %v", image.ZP, window)
 	}
+}
 
+func TestWindowAt2(t *testing.T) {
+
+	window, _ := windowAt(image.Point{1, 1})
+	neighbourCount := 8
+
+	if len(window) != neighbourCount {
+		t.Errorf("Expected %v image points, but got %v", neighbourCount, len(window))
+	}
 }
