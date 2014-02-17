a9881e7258c6f864fceb0ca2450723c19e6215f3
Felix Geller <fgeller@gmail.com>
2014-02-17 18:22:06 +1300

    Basic test for windowAt.

--- corner_detection_test.go
+++ corner_detection_test.go
@@ -1,17 +1,26 @@
 package cornerdetection
 
-import "testing"
+import "image"
 import "path/filepath"
+import "testing"
 
 func TestLoadImage(t *testing.T) {
 	path := filepath.FromSlash("resources/images/5x5-clear.png")
-	image := LoadImage(path)
-	if image == nil {
-		t.Errorf("LoadImage(%v) = %v, want image", path, image)
+	i := LoadImage(path)
+	if i == nil {
+		t.Errorf("LoadImage(%v) = %v, want image", path, i)
 	}
-	bounds := image.Bounds()
+	bounds := i.Bounds()
 	if bounds.Max.X != 5 || bounds.Max.Y != 5 {
 		t.Errorf("Unexpected image max bounds: %v", bounds)
+	}
+}
+
+func TestWindowAt(t *testing.T) {
+
+	window := windowAt(image.ZP)
+	if window != nil {
+		t.Errorf("window at %v should not exist, but got %v", image.ZP, window)
 	}
 
 }
